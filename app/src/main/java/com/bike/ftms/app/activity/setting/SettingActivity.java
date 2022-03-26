6e7eb340843aba867d2fcc00387c19d5ab282d45
zhangriguang <zhangriguang@chuhui.com>
2022-03-26 11:30:32 +0800

    1.设置界面竖屏ok

--- app/src/main/java/com/bike/ftms/app/activity/setting/SettingActivity.java
+++ app/src/main/java/com/bike/ftms/app/activity/setting/SettingActivity.java
@@ -1,29 +1,35 @@
 package com.bike.ftms.app.activity.setting;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.bike.ftms.app.Debug;
 import com.bike.ftms.app.R;
+import com.bike.ftms.app.activity.OnOrientationChanged;
 import com.bike.ftms.app.activity.bluetooth.BluetoothActivity;
 import com.bike.ftms.app.activity.bluetooth.HeartRateMonitorActivity;
 import com.bike.ftms.app.activity.user.LoginActivity;
 import com.bike.ftms.app.activity.user.UserManager;
 import com.bike.ftms.app.base.BaseActivity;
+import com.bike.ftms.app.utils.Logger;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import tech.gujin.toast.ToastUtil;
 
-public class SettingActivity extends BaseActivity {
+public class SettingActivity extends BaseActivity implements OnOrientationChanged {
     private static final String TAG = SettingActivity.class.getSimpleName();
 
     private final int HR_REQUEST_CODE = 100;
 
     @BindView(R.id.iv_setting_login)
     ImageView iv_setting_login;
+    @BindView(R.id.ll_setting)
+    LinearLayout ll_setting;
 
     @Override
     protected String getTAG() {
@@ -33,6 +39,33 @@ public class SettingActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Configuration cf = this.getResources().getConfiguration(); //获取设置的配置信息
+        int ori = cf.orientation; //获取屏幕方向
+        Logger.i("横竖屏: " + ori);
+        setOri(ori);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Logger.i("newConfig 横竖屏 " + newConfig.orientation);
+        setOri(newConfig.orientation);
+    }
+
+    private void setOri(int ori) {
+        if (ori == Configuration.ORIENTATION_LANDSCAPE) {
+            //横屏
+            setLandLayout();
+        } else if (ori == Configuration.ORIENTATION_PORTRAIT) {
+            //竖屏
+            setPortLayout();
+        }
     }
 
     @Override
@@ -77,5 +110,15 @@ public class SettingActivity extends BaseActivity {
                 startActivity(new Intent(this, VersionActivity.class));
                 break;
         }
+    }
+
+    @Override
+    public void setPortLayout() {
+        ll_setting.setOrientation(LinearLayout.VERTICAL);
+    }
+
+    @Override
+    public void setLandLayout() {
+        ll_setting.setOrientation(LinearLayout.HORIZONTAL);
     }
 }
